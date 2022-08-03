cc6185c8d4be390c898ba9cc806a5b5273943501
Jan Ritter <jan.ritter@trustedshops.de>
2022-08-03 16:38:55 +0200

    feat: added memory changer and first code for the main test loop

---
+++ changer/lambda.go
@@ -0,0 +1,18 @@
+package changer
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/lambda"
+	"go.uber.org/zap"
+)
+
+func (c *Changer) ChangeMemory(lambdaARN string, memory int) error {
+	_, err := c.lambda.UpdateFunctionConfiguration(&lambda.UpdateFunctionConfigurationInput{
+		MemorySize:   aws.Int64(int64(memory)),
+		FunctionName: aws.String(lambdaARN),
+	})
+	if err != nil {
+		c.logger.Error("Failed to change memory: ", zap.Error(err))
+	}
+	return err
+}
--- cmd/root.go
+++ cmd/root.go
@@ -2,21 +2,52 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
+	"log"
 	"os"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
+	"github.com/janritter/aws-lambda-live-tuner/changer"
+	"github.com/spf13/cobra"
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
+
+	"go.uber.org/zap"
 )
 
 var cfgFile string
+var minRequests int
+var memoryMin int
+var memoryMax int
+var memoryIncrement int
+var lambdaARN string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "aws-lambda-live-tuner",
 	Short: "Tool to optimize Lambda functions on real incoming events",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello world")
+		logger, _ := zap.NewDevelopment()
+		defer logger.Sync()
+		sugaredLogger := logger.Sugar()
+
+		sugaredLogger.Info("Starting AWS Lambda Live Tuner")
+		validateInputs()
+
+		awsSession := session.Must(session.NewSession())
+		lambdaSvc := lambda.New(awsSession)
+
+		changer := changer.NewChanger(lambdaSvc, sugaredLogger)
+
+		for memory := memoryMin; memory <= memoryMax; memory += memoryIncrement {
+			sugaredLogger.Infof("Starting test for %dMB", memory)
+
+			err := changer.ChangeMemory(lambdaARN, memory)
+			if err != nil {
+				os.Exit(1)
+			}
+		}
 	},
 }
 
@@ -32,15 +63,13 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.aws-lambda-live-tuner.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().IntVar(&minRequests, "min-requests", 5, "Minimum number of requests the Lambda function must receive before continuing with the next memory configuration")
+	rootCmd.PersistentFlags().IntVar(&memoryMin, "memory-min", 128, "Lower memory limit for the optimization")
+	rootCmd.PersistentFlags().IntVar(&memoryMax, "memory-max", 2048, "Upper memory limit for the optimization")
+	rootCmd.PersistentFlags().IntVar(&memoryIncrement, "memory-increment", 64, "Increments for the memory configuration added to the min value until the max value is reached. The increment must be a multiple of 64")
+	rootCmd.PersistentFlags().StringVar(&lambdaARN, "lambda-arn", "", "ARN of the Lambda function to optimize")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -66,5 +95,48 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func validateInputs() {
+	validateMemoryMinValue()
+	validateMemoryMaxValue()
+	validateMemoryIncrement()
+	validateMinRequests()
+}
+
+func validateMemoryMinValue() {
+	if memoryMin < 128 {
+		log.Println("Memory min value must be greater than or equal to 128")
+		os.Exit(1)
+	}
+	if memoryMin%64 != 0 {
+		log.Println("Memory min value must be a multiple of 64 with the minimal value of 128")
+		os.Exit(1)
+	}
+}
+
+func validateMemoryMaxValue() {
+	if memoryMax <= memoryMin {
+		log.Println("Memory max value must be greater than the min value")
+		os.Exit(1)
+	}
+	if memoryMax%64 != 0 {
+		log.Println("Memory max value must be a multiple of 64 with the minimal value of 192")
+		os.Exit(1)
+	}
+}
+
+func validateMemoryIncrement() {
+	if memoryIncrement%64 != 0 {
+		log.Println("Memory increment value must be a multiple of 64")
+		os.Exit(1)
+	}
+}
+
+func validateMinRequests() {
+	if minRequests < 1 {
+		log.Println("Minimum number of requests must be greater than 0")
+		os.Exit(1)
 	}
 }
