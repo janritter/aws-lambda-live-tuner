9fac8aba7010bea345f0ff21f44fa7a0c2261488
Jan Ritter <jan.ritter@trustedshops.de>
2022-08-04 11:48:19 +0200

    fix: reset lambda memory config to the pre test value - resolves #2

--- changer/lambda.go
+++ changer/lambda.go
@@ -17,3 +17,15 @@ func (c *Changer) ChangeMemory(lambdaARN string, memory int) error {
 	c.logger.Infof("Changed Lambda memory to: %d", memory)
 	return nil
 }
+
+func (c *Changer) GetCurrentMemoryValue(lambdaARN string) (int, error) {
+	result, err := c.lambda.GetFunctionConfiguration(&lambda.GetFunctionConfigurationInput{
+		FunctionName: aws.String(lambdaARN),
+	})
+	if err != nil {
+		c.logger.Error("Failed to get current memory value: ", zap.Error(err))
+		return -1, err
+	}
+
+	return int(*result.MemorySize), nil
+}
