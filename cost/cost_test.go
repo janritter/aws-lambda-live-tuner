00575a4b257aee907ee807ccc589db9e3810ab17
Jan Ritter <jan.ritter@trustedshops.de>
2022-09-10 14:05:31 +0200

    feat(#15): added price support for all AWS regions - resolves #15

--- cost/cost_test.go
+++ cost/cost_test.go
@@ -5,9 +5,31 @@ import (
 	"testing"
 )
 
-func TestCalculateCost(t *testing.T) {
+func TestCalculateCostX86(t *testing.T) {
 	expected := 0.000021333376
 	actual := Calculate(1280.0, 1024, "x86_64", "eu-central-1")
+
+	log.Println(actual)
+
+	if actual != expected {
+		t.Errorf("expected %f, got %f", expected, actual)
+	}
+}
+
+func TestCalculateCostARM(t *testing.T) {
+	expected := 0.0000133334
+	actual := Calculate(1000.0, 1024, "arm64", "eu-west-1")
+
+	log.Println(actual)
+
+	if actual != expected {
+		t.Errorf("expected %f, got %f", expected, actual)
+	}
+}
+
+func TestCalculateCostInvalidRegion(t *testing.T) {
+	expected := 0.0
+	actual := Calculate(1000.0, 1024, "arm64", "unkown")
 
 	log.Println(actual)
 
