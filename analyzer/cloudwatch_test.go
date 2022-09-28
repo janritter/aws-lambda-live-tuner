803af7cf8455d8f4f5985dcf697b9fe791c2948f
Jan Ritter <git@janrtr.de>
2022-09-28 21:51:14 +0200

    test: added additional unit tests for analyzer

--- analyzer/cloudwatch_test.go
+++ analyzer/cloudwatch_test.go
@@ -1,27 +1,44 @@
 package analyzer
 
-import "testing"
+import (
+	"testing"
 
-func TestGetDurationFromMessage(t *testing.T) {
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetDurationWithRequestIdFromMessage(t *testing.T) {
 	message := "REPORT RequestId: 7239847892374-7238947823947823-234234324\tDuration: 189.12 ms\tBilled Duration: 190 ms\tMemory Size: 256 MB\tMax Memory Used: 67 MB\t\n"
-	expected := 189.12
-	actual, err := getDurationFromMessage(message)
-	if err != nil {
-		t.Errorf("expected nil, got %s", err)
-	}
-	if actual != expected {
-		t.Errorf("expected %f, got %f", expected, actual)
-	}
+	expectedDuration := 189.12
+	expectedRequestId := "7239847892374-7238947823947823-234234324"
+
+	requestId, duration, err := getDurationWithRequestIdFromMessage(message)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedDuration, duration)
+	assert.Equal(t, expectedRequestId, requestId)
 }
 
-func TestGetRequestIdFromMessage(t *testing.T) {
-	message := "REPORT RequestId: 7239847892374-7238947823947823-234234324\tDuration: 189.12 ms\tBilled Duration: 190 ms\tMemory Size: 256 MB\tMax Memory Used: 67 MB\t\n"
-	expected := "7239847892374-7238947823947823-234234324"
-	actual, err := getRequestIdFromMessage(message)
-	if err != nil {
-		t.Errorf("expected nil, got %s", err)
-	}
-	if actual != expected {
-		t.Errorf("expected %s, got %s", expected, actual)
-	}
+func TestGetDurationWithRequestIdFromMessageNoDuration(t *testing.T) {
+	message := "REPORT RequestId: 7239847892374-7238947823947823-234234324\tDuration \tMemory Size: 256 MB\tMax Memory Used: 67 MB\t\n"
+
+	requestId, duration, err := getDurationWithRequestIdFromMessage(message)
+	assert.Error(t, err)
+	assert.Equal(t, -1.0, duration)
+	assert.Equal(t, "", requestId)
+}
+
+func TestGetDurationWithRequestIdFromMessageNoRequestId(t *testing.T) {
+	message := "REPORT Duration: 189.12 ms\tBilled Duration: 190 ms\tMemory Size: 256 MB\tMax Memory Used: 67 MB\t\n"
+
+	requestId, duration, err := getDurationWithRequestIdFromMessage(message)
+	assert.Error(t, err)
+	assert.Equal(t, -1.0, duration)
+	assert.Equal(t, "", requestId)
+}
+
+func TestGetStartTimeDiff(t *testing.T) {
+	assert.Equal(t, 300, getStartTimeDiff(150))
+
+	input := 301
+	expected := input * 2
+	assert.Equal(t, expected, getStartTimeDiff(input))
 }
--- analyzer/main_test.go
+++ analyzer/main_test.go
@@ -1,12 +1,25 @@
 package analyzer
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/janritter/aws-lambda-live-tuner/mocks"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestGetFunctionNameFromARN(t *testing.T) {
 	arn := "arn:aws:lambda:us-east-1:123456789012:function:my-function"
 	expected := "my-function"
 	actual := getFunctionNameFromARN(arn)
-	if actual != expected {
-		t.Errorf("expected %s, got %s", expected, actual)
-	}
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestNewLambda(t *testing.T) {
+	cloudwatch := new(mocks.CloudWatchLogsAPI)
+
+	service := NewAnalyzer(cloudwatch, "arn:aws:test", 0)
+
+	assert.NotEmpty(t, service, "Expected to be not empty")
+	assert.NotNil(t, service, "Expected not to be nil")
 }
